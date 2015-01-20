e9ffd89bbe0340be80c4972cefb37b82d8462620
nelson <zhouge@mogujie.com>
2015-01-20 22:14:52 +0800

    done

--- generate_data.py
+++ generate_data.py
@@ -7,10 +7,20 @@ def generate_dictionaries(infile,outfile):
     print("dictionary save done!:\n"+dictionary.token2id)
     return dictionary
 
-def generate_corpus(infile,outfile1,outfile2):
+def generate_corpus(raw_file,dict_file,bow_file,tfidf_file):
+    dicionary=generate_dictionaries(raw_file,dict_file)
+    corpus=[dicionary.doc2bow(line.lower().split()) for line in open(raw_file)]
+    gensim.corpora.MmCorpus.serialize(bow_file, corpus)
+    print("bow file save done!")
+
+    tfidf = gensim.models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+    gensim.corpora.MmCorpus.serialize(tfidf_file,corpus_tfidf)
+    print("tfidf file save done!")
 
 
 
 if __name__=='__main__':
     args=sys.argv
     print(args)
+    generate_corpus(args[1],args[2],args[3])
--- train_lda_model.py
+++ train_lda_model.py
@@ -4,7 +4,7 @@ __author__ = 'nelson'
 def train_model(dict_file,tfidf_file,model_file,n):
     dictionary = gensim.corpora.dictionary.Dictionary.load(dict_file)
     tfidf = gensim.corpora.MmCorpus(tfidf_file)
-    lda = gensim.models.ldamodel.LdaModel(corpus=tfidf, id2word=dictionary, num_topics=n, alpha='auto', eval_every=0, passes=20)
+    lda = gensim.models.LdaMulticore(corpus=tfidf,num_topics=n,id2word=dictionary,chunksize=10000,passes=20,batch=False,alpha='auto')
     lda.save(model_file)
     f = codecs.open('topics','a','utf-8')
     f.write("\n".join(lda.print_topics(n)))
