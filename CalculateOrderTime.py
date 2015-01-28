7e627fae32a6fef6324fbe6aae3283a33bc928e8
nelson <zhouge@mogujie.com>
2015-01-28 17:18:18 +0800

    cal order time

---
+++ CalculateOrderTime.py
@@ -0,0 +1,27 @@
+import sys
+import time
+import datetime
+
+__author__ = 'nelson'
+
+def calOrderTime(infile,outfile):
+    writer = open(outfile,'w')
+    for line in open(infile):
+        parts = line.split(',')
+        uid = parts[0]
+        length = len(parts)
+        total = 0.0
+        for i in xrange(1,length-2):
+            d1 = datetime.datetime.strptime(parts[i],'%Y%m%d')
+            d2 = datetime.datetime.strptime(parts[i+1],'%Y%m%d')
+            total += (d2-d1).days
+        avg = total/(length-2)
+        writer.write(str(uid)+' '+str(avg)+'\n')
+        writer.flush()
+    writer.flush()
+    writer.close()
+
+if __name__ == '__main__':
+    args = sys.argv
+    print(args)
+    calOrderTime(args[1],args[2])
